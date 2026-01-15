48cd2ee396de24a2dd93dc431704301ed249cc25
Sneha Sharma <gautamisharma520@gmail.com>
2026-01-15 13:05:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -35,6 +35,8 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(5000, () => {
-  console.log('Socket server running on http://localhost:5000')
-})
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Socket server running on port ${PORT}`)
+});
